2c5132ae8817d4ef0d4a3248fd750a15d6a1086c
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-04-03 11:05:38 -0300

    fecho

---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+  <div class="contact">
+    <h1>This is an contact page</h1>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
